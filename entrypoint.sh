b6ec727dfea06919b76f2c672626870aab110687
Hugogerva <hgervasoni@protonmail.com>
2018-11-15 10:03:30 +0100

    removed timeout

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,8 @@
+#!/bin/sh
 
-#!/bin/bash
-
-# autre option de pool: pool.minexmr.com:4444
-POOL=xmrpool.eu
-PORT=3333
-ADDRESS=45KHmgAb8ipJrYahAsWyYnRHY9mzpWhPLjiHofpwEJMWJXieb6t96inU6D7uXrwa2aiHcdzyfSWrVCU2CTKWpvsV1DLciiS
-
-DONATE_LEVEL=1
-THREADS=3
-
-(xmrig -o $POOL:$PORT -u $ADDRESS -p x --donate-level=$DONATE_LEVEL) & pid=$!
-sleep $1 && kill -SIGINT $pid
+args=$@
+echo "start xmrig with arg : ${args}" 
+mkdir -p iexec
+xmrig ${args}
+echo "mined" >> iexec/consensus.iexec
+cat /iexec/consensus.iexec
